0a992d95383ada6e903007e2f90ee37679606217
Scarabyte <Scarabyte@users.noreply.github.com>
2016-05-25 19:42:46 -0400

    Update SSW555-Project02-Burbidge.py
    Add command to print level number (first character of the line).
    File also specifies assumptions given in the project overview.

--- SSW555-Project02-Burbidge.py
+++ SSW555-Project02-Burbidge.py
@@ -11,3 +11,12 @@ except:
 
 for line in filehandle:
     print line,
+
+    # Assumptions (stated in Project Overview):
+    # -GEDCOM files are syntactically correct
+    #  -Files start with a level number as the first character
+    #  -Files have a legal tag
+    #  -All arguments are in the proper format
+    #  -One blank space separates the fields (level, tag, arguments)
+
+    print line[0]

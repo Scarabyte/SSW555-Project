ae3638b054b1162a936d176b1a4fa5effa8afc2e
Scarabyte <Scarabyte@users.noreply.github.com>
2016-05-25 20:57:44 -0400

    Update SSW555-Project02-Burbidge.py
    Add logic to print the valid flags as defined in the Project Overview file.

--- SSW555-Project02-Burbidge.py
+++ SSW555-Project02-Burbidge.py
@@ -8,9 +8,12 @@ except:
     print 'Invalid file name: ', fname
     exit()
 
+# Define the valid GEDCOM flags for this project
+valid_tags = ["INDI", "NAME", "SEX", "BIRT", "DEAT", "FAMC", "FAMS", "FAM",
+              "MARR", "HUSB", "WIFE", "CHIL", "DIV", "DATE", "HEAD", "TRLR", "NOTE"]
 
 for line in filehandle:
-    print line,
+    print line, # The line itself
 
     # Assumptions (stated in Project Overview):
     # -GEDCOM files are syntactically correct
@@ -19,4 +22,26 @@ for line in filehandle:
     #  -All arguments are in the proper format
     #  -One blank space separates the fields (level, tag, arguments)
 
-    print line[0]
+    print line[0] # The level number, which we assumed to be the first character
+    # (Note that we could also do this after we separate the line below.)
+
+    # Separate the line into three parts: Level, Tag, Arguments
+    # Each field is separated by a single space.
+    # Use -1 as the index to remove the newline character.
+    # Also note that not all tags have an argument. But for this assignment,
+    # we're not required to do anything with the argument, just print the tag.
+
+    separated_line = line[:-1].split(' ', 2)
+    level = separated_line[0]
+    tag = separated_line[1]
+
+    # If the line's Tag is one of the valid tags we identified for this project, print it.
+    # If not, print "Invalid tag"
+
+    if tag in valid_tags:
+        print tag
+    else:
+        print "Invalid tag"
+
+    # Just as a separator to make the output easier to read, let's add a newline.
+    print
